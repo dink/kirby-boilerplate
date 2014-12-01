88b5f9246b9f9c3302c71e410f9d1d0e7b27feb6
Christian Zehetner <mail@christianzehetner.at>
2014-12-01 13:11:09 +0100

    new kirby version, added update script.

--- site/accounts/admin.php
+++ site/accounts/admin.php
@@ -5,4 +5,4 @@ email: admin@madergrafisch.at
 password: >
   $2a$10$e9OXSfsRsgL4g/TYmqCFX.iz1.b3k84PK8AsqizNt9g6P2Q0Zhj4K
 language: de
-token: c8fd03131f9145e7a30c54535d4d505844853874
+token: 3508c38279a5fe3e9979f568904085967d730afa
