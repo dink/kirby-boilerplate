581a9b40d009c6b4c3d14a80f72deee14bac3205
Christian Zehetner <mail@christianzehetner.at>
2015-03-12 08:47:09 +0100

    Remove admin.php from git index.

--- site/accounts/admin.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: admin
-email: admin@madergrafisch.at
-password: >
-  $2a$10$e9OXSfsRsgL4g/TYmqCFX.iz1.b3k84PK8AsqizNt9g6P2Q0Zhj4K
-language: de
-token: 3508c38279a5fe3e9979f568904085967d730afa
